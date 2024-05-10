680114ac31242562f0784aca6548e05cc377dbd1
Yulia Magonya <ymagonia@gmail.com>
2024-05-11 01:12:53 +0300

    hw1

---
+++ LECT8_HW1.py
@@ -0,0 +1,29 @@
+def skip_if(condition, reason):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if not condition:
+                return func(*args, **kwargs)
+            else:
+                print(reason)
+
+        return wrapper
+
+    return decorator
+
+
+@skip_if(True, "Some reason...")
+def test_one():
+    assert 2 + 2 == 4
+    print("Test one.")
+
+
+test_one()
+
+
+@skip_if(False, "Some reason...")
+def test_two():
+    assert 2 - 2 == 0
+    print("Test two.")
+
+
+test_two()
